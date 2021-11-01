fdd155c0e400c9474bec1dd65596471fe62901a2
monishachy09 <monishachy09@gmail.com>
2021-11-01 16:20:11 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient  } = require('mongodb');
+require('dotenv').config()
+
+const port = process.env.PORT || 4000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xxcl9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('package_info');
+        const courseCollection = database.collection('courses');
+
+        // Get API //
+
+        app.get('/course', async (req, res) => {
+            const cursor = courseCollection.find({});
+            const courses = await cursor.toArray();
+            res.send(courses);
+        })
+
+
+        //single data api
+        app.get('/courseSingle/:id', async (req, res) => {
+            const courseSingle = await courseCollection.findOne({ _id:(req.params.id) })
+            res.json(courseSingle);
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+
+run().catch(console.dir);
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+});
+
+app.listen(port, () => {
+    console.log('server running at port', port);
+})
